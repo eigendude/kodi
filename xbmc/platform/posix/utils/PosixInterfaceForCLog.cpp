9f0fafb45e34258012121f8bc7e42e476d207d47
Garrett Brown <themagnificentmrb@gmail.com>
2020-03-18 16:58:40 -0700

    [posix] Fix error cross-compiling for Android
    Fix the error by removing the forward declaration and unwraping the FILE
    struct.
    
    Error was:
    
    | PosixInterfaceForCLog.cpp:20:26: error: base class has incomplete type
    | struct FILEWRAP : public FILE
    |                   ~~~~~~~^~~~

--- xbmc/platform/posix/utils/PosixInterfaceForCLog.cpp
+++ xbmc/platform/posix/utils/PosixInterfaceForCLog.cpp
@@ -7,7 +7,6 @@
  */
 
 #include "PosixInterfaceForCLog.h"
-#include <stdio.h>
 #include <time.h>
 #include <sys/time.h>
 
@@ -16,10 +15,6 @@
 #elif defined(TARGET_ANDROID)
 #include "platform/android/activity/XBMCApp.h"
 #endif // TARGET_ANDROID
-
-struct FILEWRAP : public FILE
-{};
-
 
 CPosixInterfaceForCLog::CPosixInterfaceForCLog() :
   m_file(NULL)
@@ -40,7 +35,7 @@ bool CPosixInterfaceForCLog::OpenLogFile(const std::string &logFilename, const s
   (void)remove(backupOldLogToFilename.c_str()); // if it's failed, try to continue
   (void)rename(logFilename.c_str(), backupOldLogToFilename.c_str()); // if it's failed, try to continue
 
-  m_file = (FILEWRAP*)fopen(logFilename.c_str(), "wb");
+  m_file = fopen(logFilename.c_str(), "wb");
   if (!m_file)
     return false; // error, can't open log file
 
