185507394e635bb46315bee4ae21024556ff4a05
Garrett Brown <themagnificentmrb@gmail.com>
2021-05-30 02:53:08 -0700

    Smart Home: Camera and input streaming

--- xbmc/ServiceBroker.cpp
+++ xbmc/ServiceBroker.cpp
@@ -489,3 +489,8 @@ std::shared_ptr<XFILE::CBlurayDiscCache> CServiceBroker::GetBlurayDiscCache()
 {
   return g_serviceBroker.m_blurayDiscCache;
 }
+
+SMART_HOME::CSmartHomeServices& CServiceBroker::GetSmartHomeServices()
+{
+  return g_application.m_ServiceManager->GetSmartHomeServices();
+}
