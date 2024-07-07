ec9bdcf538cc90cfbfc69a177a48a2f7ece681cb
GTechAlpha <31323818+GTechAlpha@users.noreply.github.com>
2024-07-06 21:24:41 -0500

    RetroPlayer: Fix gameloop
    Includes several improvements:
    
      * Document CGameLoop and IGameLoopCallback
      * Improve initialization of member variables
      * Break += statement into two lines

--- xbmc/cores/RetroPlayer/playback/GameLoop.cpp
+++ xbmc/cores/RetroPlayer/playback/GameLoop.cpp
@@ -15,84 +15,106 @@ using namespace KODI;
 using namespace RETRO;
 using namespace std::chrono_literals;
 
-#define DEFAULT_FPS 60 // In case fps is 0 (shouldn't happen)
-#define FOREVER_MS (7 * 24 * 60 * 60 * 1000) // 1 week is large enough
+namespace
+{
+// Default FPS value in case fps is 0 (which shouldn't happen)
+constexpr double DEFAULT_FPS = 60.0;
+} // namespace
 
 CGameLoop::CGameLoop(IGameLoopCallback* callback, double fps)
-  : CThread("GameLoop"), m_callback(callback), m_fps(fps ? fps : DEFAULT_FPS), m_speedFactor(0.0)
+  : CThread("GameLoop"), m_callback(callback), m_fps(fps ? fps : DEFAULT_FPS)
 {
 }
 
 CGameLoop::~CGameLoop()
 {
+  // Ensure the game loop is stopped when the object is destroyed
   Stop();
 }
 
 void CGameLoop::Start()
 {
+  // Start the thread by invoking the base class's Create method
   Create();
 }
 
 void CGameLoop::Stop()
 {
+  // Request to stop the thread
   StopThread(false);
+
+  // Wake up the thread if it's waiting on the sleep event
   m_sleepEvent.Set();
+
+  // Wait until the thread is fully stopped
   StopThread(true);
 }
 
 void CGameLoop::SetSpeed(double speedFactor)
 {
+  // Set the new speed factor
   m_speedFactor = speedFactor;
 
+  // Wake up the thread to apply the new speed immediately
   m_sleepEvent.Set();
 }
 
 void CGameLoop::PauseAsync()
 {
+  // Pause the game loop asynchronously by setting speed to 0
   SetSpeed(0.0);
 }
 
 void CGameLoop::Process(void)
 {
+  // Main game loop that runs until the thread is requested to stop
   while (!m_bStop)
   {
-    if (m_speedFactor == 0.0)
+    // If the speed factor has changed, reset the last frame time and update
+    // the loop speed factor
+    if (m_loopSpeedFactor != m_speedFactor)
     {
-      m_lastFrameMs = 0.0;
+      // Reset last frame time
+      m_lastFrameUs = std::chrono::microseconds::zero();
+
+      // Update loop speed factor
+      m_loopSpeedFactor = m_speedFactor;
+    }
+
+    // If the game is paused, wait for 5000 milliseconds before checking again
+    if (m_loopSpeedFactor == 0.0)
+    {
+      // Wait for a long duration (5 seconds) if paused
       m_sleepEvent.Wait(5000ms);
     }
     else
     {
-      if (m_speedFactor > 0.0)
+      // If it's the first frame or after a reset, set the last frame time to
+      // the current time
+      if (m_lastFrameUs == std::chrono::microseconds::zero())
+        m_lastFrameUs = NowUs();
+
+      // Trigger the appropriate callback depending on the direction of time
+      // (forward or rewind)
+      if (m_loopSpeedFactor > 0.0)
         m_callback->FrameEvent();
-      else if (m_speedFactor < 0.0)
+      else if (m_loopSpeedFactor < 0.0)
         m_callback->RewindEvent();
 
-      if (m_lastFrameMs > 0.0)
-      {
-        m_lastFrameMs += FrameTimeMs();
-        m_adjustTime = m_lastFrameMs - NowMs();
-      }
-      else
-      {
-        m_lastFrameMs = NowMs();
-        m_adjustTime = 0.0;
-      }
+      // Calculate the time for the next frame by adding the frame duration to
+      // the last frame time
+      const std::chrono::microseconds nextFrameUs = m_lastFrameUs + FrameTimeUs();
 
-      // Calculate sleep time
-      double sleepTimeMs = SleepTimeMs();
+      // Update the last frame time to ensure accurate timing for the next loop
+      // iteration
+      m_lastFrameUs = nextFrameUs;
 
-      // Sleep at least 1 ms to avoid sleeping forever
-      while (sleepTimeMs > 1.0)
-      {
-        m_sleepEvent.Wait(std::chrono::milliseconds(static_cast<unsigned int>(sleepTimeMs)));
+      // Calculate how much time to sleep before the next frame should be processed
+      const std::chrono::microseconds sleepTimeUs = (nextFrameUs - NowUs());
 
-        if (m_bStop)
-          break;
-
-        // Speed may have changed, update sleep time
-        sleepTimeMs = SleepTimeMs();
-      }
+      // Only sleep if there is time left before the next frame
+      if (sleepTimeUs > std::chrono::microseconds::zero())
+        m_sleepEvent.Wait(sleepTimeUs);
     }
   }
 
@@ -100,34 +122,20 @@ void CGameLoop::Process(void)
   m_callback->EndEvent();
 }
 
-double CGameLoop::FrameTimeMs() const
+std::chrono::microseconds CGameLoop::FrameTimeUs() const
 {
-  if (m_speedFactor != 0.0)
-    return 1000.0 / m_fps / std::abs(m_speedFactor);
+  // Calculate the duration of one frame in microseconds based on the FPS and
+  // speed factor
+  if (m_loopSpeedFactor != 0.0)
+    return std::chrono::duration_cast<std::chrono::microseconds>(1s / m_fps /
+                                                                 std::abs(m_loopSpeedFactor));
   else
-    return 1000.0 / m_fps / 1.0;
+    return std::chrono::duration_cast<std::chrono::microseconds>(1s / m_fps);
 }
 
-double CGameLoop::SleepTimeMs() const
+std::chrono::microseconds CGameLoop::NowUs() const
 {
-  // Calculate next frame time
-  const double nextFrameMs = m_lastFrameMs + FrameTimeMs();
-
-  // Calculate sleep time
-  double sleepTimeMs = (nextFrameMs - NowMs()) + m_adjustTime;
-
-  // Reset adjust time
-  m_adjustTime = 0.0;
-
-  // Positive or zero
-  sleepTimeMs = (sleepTimeMs >= 0.0 ? sleepTimeMs : 0.0);
-
-  return sleepTimeMs;
-}
-
-double CGameLoop::NowMs() const
-{
-  return std::chrono::duration<double, std::milli>(
-             std::chrono::steady_clock::now().time_since_epoch())
-      .count();
+  // Get the current time in microseconds since the steady clock epoch
+  return std::chrono::duration_cast<std::chrono::microseconds>(
+      std::chrono::steady_clock::now().time_since_epoch());
 }
