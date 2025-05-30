141ce9cb45c4d4e47ba5f07160d06ec278799475
Garrett Brown <garrett.brown@aclima.io>
2025-05-30 06:07:04 -0700

    GL: unbind element array buffer after rendering

--- xbmc/cores/RetroPlayer/rendering/VideoRenderers/RPRendererOpenGLES.cpp
+++ xbmc/cores/RetroPlayer/rendering/VideoRenderers/RPRendererOpenGLES.cpp
@@ -234,6 +234,7 @@ void CRPRendererOpenGLES::DrawBlackBars()
 
   glDisableVertexAttribArray(posLoc);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
   m_context.DisableGUIShader();
 }
