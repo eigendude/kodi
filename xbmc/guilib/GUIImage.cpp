garbear/xbmc

[WIP PR 24395] guilib: Allow textures to use nearest-neighbor filtering

xbmc/guilib/GUIImage.cpp
@@ -320,6 +320,11 @@ void CGUIImage::SetAspectRatio(const CAspectRatio &aspect)
   m_texture->SetAspectRatio(aspect);
 }
 
+void CGUIImage::SetScalingMethod(TEXTURE_SCALING scalingMethod)
+{
+  m_texture->SetScalingMethod(scalingMethod);
+}
+
 void CGUIImage::SetCrossFade(unsigned int time)
 {
   m_crossFadeTime = time;

eigendude/kodi

xbmc/guilib/GUIImage.cpp
@@ -409,6 +409,12 @@ void CGUIImage::SetAspectRatio(const CAspectRatio &aspect)
   m_textureNext->SetAspectRatio(aspect);
 }
 
+void CGUIImage::SetScalingMethod(TEXTURE_SCALING scalingMethod)
+{
+  m_textureCurrent->SetScalingMethod(scalingMethod);
+  m_textureNext->SetScalingMethod(scalingMethod);
+}
+
 void CGUIImage::SetCrossFade(unsigned int time)
 {
   m_crossFadeTime = time;