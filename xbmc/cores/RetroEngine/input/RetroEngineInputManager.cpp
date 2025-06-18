garbear/xbmc

[WIP] RetroEngine: A new player core for kodi
This includes two subsystems:

  * Rendering
  * Input

xbmc/cores/RetroEngine/input/RetroEngineInputManager.cpp
@@ -1,0 +1,114 @@
+/*
+ *  Copyright (C) 2022-2024 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#include "RetroEngineInputManager.h"
+
+#include "RetroEngineJoystick.h"
+#include "games/GameServices.h"
+#include "peripherals/Peripherals.h"
+
+using namespace KODI;
+using namespace RETRO_ENGINE;
+
+CRetroEngineInputManager::CRetroEngineInputManager(PERIPHERALS::CPeripherals& peripheralManager)
+  : m_peripheralManager(peripheralManager)
+{
+}
+
+CRetroEngineInputManager::~CRetroEngineInputManager() = default;
+
+void CRetroEngineInputManager::Initialize(GAME::CGameServices& gameServices)
+{
+  m_gameServices = &gameServices;
+}
+
+void CRetroEngineInputManager::Deinitialize()
+{
+  m_gameServices = nullptr;
+}
+
+void CRetroEngineInputManager::RegisterPeripheralObserver(Observer* obs)
+{
+  m_peripheralManager.RegisterObserver(obs);
+}
+
+void CRetroEngineInputManager::UnregisterPeripheralObserver(Observer* obs)
+{
+  m_peripheralManager.UnregisterObserver(obs);
+}
+
+PERIPHERALS::PeripheralVector CRetroEngineInputManager::GetPeripherals() const
+{
+  PERIPHERALS::PeripheralVector peripherals;
+
+  m_peripheralManager.GetPeripheralsWithFeature(peripherals, PERIPHERALS::FEATURE_KEYBOARD);
+  m_peripheralManager.GetPeripheralsWithFeature(peripherals, PERIPHERALS::FEATURE_MOUSE);
+  m_peripheralManager.GetPeripheralsWithFeature(peripherals, PERIPHERALS::FEATURE_JOYSTICK);
+
+  return peripherals;
+}
+
+void CRetroEngineInputManager::PrunePeripherals()
+{
+  PERIPHERALS::PeripheralVector peripherals = GetPeripherals();
+  for (auto it = m_joysticks.begin(); it != m_joysticks.end();)
+  {
+    // Dereference iterator
+    const std::string& peripheralLocation = it->first;
+
+    // Check if peripheral is in the list of connected peripherals
+    auto it2 = std::find_if(peripherals.begin(), peripherals.end(),
+                            [peripheralLocation](const PERIPHERALS::PeripheralPtr& peripheral) {
+                              return peripheral->Location() == peripheralLocation;
+                            });
+
+    if (it2 == peripherals.end())
+    {
+      // Remove peripheral
+      it = m_joysticks.erase(it);
+    }
+    else
+    {
+      // Check next peripheral
+      ++it;
+    }
+  }
+}
+
+bool CRetroEngineInputManager::OpenJoystick(const std::string& peripheralLocation,
+                                            const std::string& controllerProfile,
+                                            IRetroEngineJoystickHandler& joystickHandler)
+{
+  // Return success if joystick is already open
+  if (m_joysticks.find(peripheralLocation) != m_joysticks.end())
+    return true;
+
+  PERIPHERALS::PeripheralPtr peripheral =
+      m_peripheralManager.GetPeripheralAtLocation(peripheralLocation);
+  if (!peripheral)
+    return false;
+
+  if (m_gameServices == nullptr)
+    return false;
+
+  const GAME::ControllerPtr controller = m_gameServices->GetController(controllerProfile);
+  if (!controller)
+    return false;
+
+  m_joysticks[peripheralLocation] = std::make_unique<CRetroEngineJoystick>(
+      std::move(peripheral), std::move(controller), joystickHandler);
+
+  return true;
+}
+
+void CRetroEngineInputManager::CloseJoystick(const std::string& peripheralLocation)
+{
+  auto it = m_joysticks.find(peripheralLocation);
+  if (it != m_joysticks.end())
+    m_joysticks.erase(it);
+}

xbmc/cores/RetroEngine/input/RetroEngineInputManager.h
@@ -1,0 +1,72 @@
+/*
+ *  Copyright (C) 2022-2024 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include "games/controllers/ControllerTypes.h"
+#include "peripherals/PeripheralTypes.h"
+
+#include <map>
+#include <memory>
+
+class Observer;
+
+namespace PERIPHERALS
+{
+class CPeripherals;
+}
+
+namespace KODI
+{
+namespace GAME
+{
+class CGameServices;
+}
+
+namespace RETRO_ENGINE
+{
+class CRetroEngineJoystick;
+class IRetroEngineJoystickHandler;
+
+class CRetroEngineInputManager
+{
+public:
+  CRetroEngineInputManager(PERIPHERALS::CPeripherals& peripheralManager);
+  ~CRetroEngineInputManager();
+
+  void Initialize(GAME::CGameServices& gameServices);
+  void Deinitialize();
+
+  void RegisterPeripheralObserver(Observer* obs);
+  void UnregisterPeripheralObserver(Observer* obs);
+
+  PERIPHERALS::PeripheralVector GetPeripherals() const;
+
+  void PrunePeripherals();
+
+  bool OpenJoystick(const std::string& peripheralLocation,
+                    const std::string& controllerProfile,
+                    IRetroEngineJoystickHandler& joystickHandler);
+  void CloseJoystick(const std::string& peripheralLocation);
+
+private:
+  using PeripheralAddress = std::string;
+  using JoystickMap = std::map<PeripheralAddress, std::unique_ptr<CRetroEngineJoystick>>;
+
+  // Construction parameters
+  PERIPHERALS::CPeripherals& m_peripheralManager;
+
+  // Initialization parameters
+  GAME::CGameServices* m_gameServices{nullptr};
+
+  // Input parameters
+  JoystickMap m_joysticks;
+};
+
+} // namespace RETRO_ENGINE
+} // namespace KODI

eigendude/kodi

xbmc/cores/RetroEngine/input/RetroEngineInputManager.cpp
@@ -1,0 +1,113 @@
+/*
+ *  Copyright (C) 2022-2024 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#include "RetroEngineInputManager.h"
+
+#include "RetroEngineJoystick.h"
+#include "games/GameServices.h"
+#include "peripherals/Peripherals.h"
+
+using namespace KODI;
+using namespace RETRO_ENGINE;
+
+CRetroEngineInputManager::CRetroEngineInputManager(PERIPHERALS::CPeripherals& peripheralManager)
+  : m_peripheralManager(peripheralManager)
+{
+}
+
+CRetroEngineInputManager::~CRetroEngineInputManager() = default;
+
+void CRetroEngineInputManager::Initialize(GAME::CGameServices& gameServices)
+{
+  m_gameServices = &gameServices;
+}
+
+void CRetroEngineInputManager::Deinitialize()
+{
+  m_gameServices = nullptr;
+}
+
+void CRetroEngineInputManager::RegisterPeripheralObserver(Observer* obs)
+{
+  m_peripheralManager.RegisterObserver(obs);
+}
+
+void CRetroEngineInputManager::UnregisterPeripheralObserver(Observer* obs)
+{
+  m_peripheralManager.UnregisterObserver(obs);
+}
+
+PERIPHERALS::PeripheralVector CRetroEngineInputManager::GetPeripherals() const
+{
+  PERIPHERALS::PeripheralVector peripherals;
+
+  m_peripheralManager.GetPeripheralsWithFeature(peripherals, PERIPHERALS::FEATURE_KEYBOARD);
+  m_peripheralManager.GetPeripheralsWithFeature(peripherals, PERIPHERALS::FEATURE_MOUSE);
+  m_peripheralManager.GetPeripheralsWithFeature(peripherals, PERIPHERALS::FEATURE_JOYSTICK);
+
+  return peripherals;
+}
+
+void CRetroEngineInputManager::PrunePeripherals()
+{
+  PERIPHERALS::PeripheralVector peripherals = GetPeripherals();
+  for (auto it = m_joysticks.begin(); it != m_joysticks.end();)
+  {
+    // Dereference iterator
+    const std::string& peripheralPath = it->first;
+
+    // Check if peripheral is in the list of connected peripherals
+    auto it2 = std::find_if(peripherals.begin(), peripherals.end(),
+                            [peripheralPath](const PERIPHERALS::PeripheralPtr& peripheral) {
+                              return peripheral->FileLocation() == peripheralPath;
+                            });
+
+    if (it2 == peripherals.end())
+    {
+      // Remove peripheral
+      it = m_joysticks.erase(it);
+    }
+    else
+    {
+      // Check next peripheral
+      ++it;
+    }
+  }
+}
+
+bool CRetroEngineInputManager::OpenJoystick(const std::string& peripheralPath,
+                                            const std::string& controllerProfile,
+                                            IRetroEngineJoystickHandler& joystickHandler)
+{
+  // Return success if joystick is already open
+  if (m_joysticks.find(peripheralPath) != m_joysticks.end())
+    return true;
+
+  PERIPHERALS::PeripheralPtr peripheral = m_peripheralManager.GetByPath(peripheralPath);
+  if (!peripheral)
+    return false;
+
+  if (m_gameServices == nullptr)
+    return false;
+
+  const GAME::ControllerPtr controller = m_gameServices->GetController(controllerProfile);
+  if (!controller)
+    return false;
+
+  m_joysticks[peripheralPath] = std::make_unique<CRetroEngineJoystick>(
+      std::move(peripheral), std::move(controller), joystickHandler);
+
+  return true;
+}
+
+void CRetroEngineInputManager::CloseJoystick(const std::string& peripheralPath)
+{
+  auto it = m_joysticks.find(peripheralPath);
+  if (it != m_joysticks.end())
+    m_joysticks.erase(it);
+}

xbmc/cores/RetroEngine/input/RetroEngineInputManager.h
@@ -1,0 +1,72 @@
+/*
+ *  Copyright (C) 2022-2024 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include "games/controllers/ControllerTypes.h"
+#include "peripherals/PeripheralTypes.h"
+
+#include <map>
+#include <memory>
+
+class Observer;
+
+namespace PERIPHERALS
+{
+class CPeripherals;
+}
+
+namespace KODI
+{
+namespace GAME
+{
+class CGameServices;
+}
+
+namespace RETRO_ENGINE
+{
+class CRetroEngineJoystick;
+class IRetroEngineJoystickHandler;
+
+class CRetroEngineInputManager
+{
+public:
+  CRetroEngineInputManager(PERIPHERALS::CPeripherals& peripheralManager);
+  ~CRetroEngineInputManager();
+
+  void Initialize(GAME::CGameServices& gameServices);
+  void Deinitialize();
+
+  void RegisterPeripheralObserver(Observer* obs);
+  void UnregisterPeripheralObserver(Observer* obs);
+
+  PERIPHERALS::PeripheralVector GetPeripherals() const;
+
+  void PrunePeripherals();
+
+  bool OpenJoystick(const std::string& peripheralPath,
+                    const std::string& controllerProfile,
+                    IRetroEngineJoystickHandler& joystickHandler);
+  void CloseJoystick(const std::string& peripheralPath);
+
+private:
+  using PeripheralAddress = std::string;
+  using JoystickMap = std::map<PeripheralAddress, std::unique_ptr<CRetroEngineJoystick>>;
+
+  // Construction parameters
+  PERIPHERALS::CPeripherals& m_peripheralManager;
+
+  // Initialization parameters
+  GAME::CGameServices* m_gameServices{nullptr};
+
+  // Input parameters
+  JoystickMap m_joysticks;
+};
+
+} // namespace RETRO_ENGINE
+} // namespace KODI