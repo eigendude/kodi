garbear/xbmc

Fix rendering controls with alpha for shader presets

xbmc/cores/RetroPlayer/rendering/VideoRenderers/RPRendererDMAOpenGL.cpp
@@ -12,6 +12,7 @@
 #include "cores/RetroPlayer/buffers/RenderBufferPoolDMA.h"
 #include "cores/RetroPlayer/rendering/RenderContext.h"
 #include "cores/RetroPlayer/shaders/gl/ShaderPresetGL.h"
+#include "cores/RetroPlayer/shaders/gl/ShaderTextureGL.h"
 #include "cores/RetroPlayer/shaders/gl/ShaderTextureGLRef.h"
 #include "utils/BufferObjectFactory.h"
 #include "utils/GLUtils.h"
@@ -56,7 +57,11 @@ void CRPRendererDMAOpenGL::Render(uint8_t alpha)
     return;
 
   RenderBufferTextures* rbTextures;
-  const auto it = m_RBTexturesMap.find(renderBuffer);
+
+  const auto key = std::tuple<CRenderBufferDMA*, unsigned int, unsigned int>{
+      renderBuffer, static_cast<unsigned int>(m_fullDestWidth),
+      static_cast<unsigned int>(m_fullDestHeight)};
+  const auto it = m_RBTexturesMap.find(key);
   if (it != m_RBTexturesMap.end())
   {
     rbTextures = it->second.get();
@@ -68,16 +73,35 @@ void CRPRendererDMAOpenGL::Render(uint8_t alpha)
         std::make_shared<SHADER::CShaderTextureGLRef>(
             static_cast<unsigned int>(renderBuffer->GetWidth()),
             static_cast<unsigned int>(renderBuffer->GetHeight()), renderBuffer->TextureID()),
-        // Target texture is empty wrapper used to pass target width and height
-        std::make_shared<SHADER::CShaderTextureGLRef>(
-            static_cast<unsigned int>(m_context.GetScreenWidth()),
-            static_cast<unsigned int>(m_context.GetScreenHeight())),
+        // Target texture
+        std::make_shared<SHADER::CShaderTextureGL>(static_cast<unsigned int>(m_fullDestWidth),
+                                                   static_cast<unsigned int>(m_fullDestHeight)),
     };
-    m_RBTexturesMap.emplace(renderBuffer, rbTextures);
+
+    std::shared_ptr<SHADER::CShaderTextureGL> target = rbTextures->target;
+
+    target->CreateTextureObject(); // Create new internal texture
+
+    const GLfloat blackBorder[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+
+    glBindTexture(m_textureTarget, target->GetTextureID());
+    glTexParameteri(m_textureTarget, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(m_textureTarget, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(m_textureTarget, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(m_textureTarget, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    glTexParameterfv(m_textureTarget, GL_TEXTURE_BORDER_COLOR, blackBorder);
+
+    // Force alpha to 1, because shader can leave it undefined
+    glTexParameteri(m_textureTarget, GL_TEXTURE_SWIZZLE_A, GL_ONE);
+
+    glTexImage2D(m_textureTarget, 0, GL_RGBA8, target->GetWidth(), target->GetHeight(), 0, GL_BGRA,
+                 GL_UNSIGNED_BYTE, nullptr);
+
+    m_RBTexturesMap.emplace(key, rbTextures);
   }
 
   std::shared_ptr<SHADER::CShaderTextureGLRef> source = rbTextures->source;
-  std::shared_ptr<SHADER::CShaderTextureGLRef> target = rbTextures->target;
+  std::shared_ptr<SHADER::CShaderTextureGL> target = rbTextures->target;
 
   Updateshaders();
 
@@ -85,7 +109,7 @@ void CRPRendererDMAOpenGL::Render(uint8_t alpha)
   if (m_bUseShaderPreset)
   {
     GLint filter = GL_NEAREST;
-    if (m_shaderPreset->GetPasses()[0].filterType == SHADER::FilterType::LINEAR)
+    if (m_shaderPreset->GetPasses().front().filterType == SHADER::FilterType::LINEAR)
       filter = GL_LINEAR;
 
     glBindTexture(m_textureTarget, source->GetTextureID());
@@ -94,76 +118,89 @@ void CRPRendererDMAOpenGL::Render(uint8_t alpha)
     glTexParameteri(m_textureTarget, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(m_textureTarget, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-    if (!m_shaderPreset->RenderUpdate(m_rotatedDestCoords, {m_fullDestWidth, m_fullDestHeight},
-                                      *source, *target))
+    if (!m_shaderPreset->RenderUpdate(*source, *target))
     {
       m_bShadersNeedUpdate = false;
       m_bUseShaderPreset = false;
     }
+
+    glActiveTexture(GL_TEXTURE0); // GUI shader samples from texture unit 0
+    glBindTexture(m_textureTarget, target->GetTextureID());
   }
-  // Use GUI shader
   else
   {
     GLint filter = GL_NEAREST;
     if (GetRenderSettings().VideoSettings().GetScalingMethod() == SCALINGMETHOD::LINEAR)
       filter = GL_LINEAR;
 
+    glActiveTexture(GL_TEXTURE0); // GUI shader samples from texture unit 0
     glBindTexture(m_textureTarget, source->GetTextureID());
     glTexParameteri(m_textureTarget, GL_TEXTURE_MAG_FILTER, filter);
     glTexParameteri(m_textureTarget, GL_TEXTURE_MIN_FILTER, filter);
     glTexParameteri(m_textureTarget, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(m_textureTarget, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-    m_context.EnableGUIShader(GL_SHADER_METHOD::TEXTURE);
-
-    GLint uniColLoc = m_context.GUIShaderGetUniCol();
-    GLint depthLoc = m_context.GUIShaderGetDepth();
-
-    // Setup color values
-    GLubyte col[4];
-    const uint32_t color = (alpha << 24) | 0xFFFFFF;
-    col[0] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::R, color);
-    col[1] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::G, color);
-    col[2] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::B, color);
-    col[3] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::A, color);
-
-    glUniform4f(uniColLoc, (col[0] / 255.0f), (col[1] / 255.0f), (col[2] / 255.0f),
-                (col[3] / 255.0f));
-    glUniform1f(depthLoc, -1.0f);
-
-    // Setup destination rectangle
-    CRect rect = m_sourceRect;
-    rect.x1 /= renderBuffer->GetWidth();
-    rect.x2 /= renderBuffer->GetWidth();
-    rect.y1 /= renderBuffer->GetHeight();
-    rect.y2 /= renderBuffer->GetHeight();
-
-    PackedVertex vertex[4];
-
-    // Setup vertex position values
-    for (unsigned int i = 0; i < 4; i++)
-    {
-      vertex[i].x = m_rotatedDestCoords[i].x;
-      vertex[i].y = m_rotatedDestCoords[i].y;
-      vertex[i].z = 0.0f;
-    }
-
-    // Setup texture coordinates
-    vertex[0].u1 = vertex[3].u1 = rect.x1;
-    vertex[0].v1 = vertex[1].v1 = rect.y1;
-    vertex[1].u1 = vertex[2].u1 = rect.x2;
-    vertex[2].v1 = vertex[3].v1 = rect.y2;
-
-    glBindVertexArray(m_mainVAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, m_mainVertexVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(PackedVertex) * 4, &vertex[0], GL_DYNAMIC_DRAW);
-
-    glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_BYTE, nullptr);
-
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    m_context.DisableGUIShader();
   }
+
+  if (alpha < 255)
+  {
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  }
+  else
+  {
+    glDisable(GL_BLEND);
+  }
+
+  // Use GUI shader
+  m_context.EnableGUIShader(GL_SHADER_METHOD::TEXTURE);
+
+  GLint uniColLoc = m_context.GUIShaderGetUniCol();
+  GLint depthLoc = m_context.GUIShaderGetDepth();
+
+  // Setup color values
+  GLubyte col[4];
+  const uint32_t color = (alpha << 24) | 0xFFFFFF;
+  col[0] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::R, color);
+  col[1] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::G, color);
+  col[2] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::B, color);
+  col[3] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::A, color);
+
+  glUniform4f(uniColLoc, (col[0] / 255.0f), (col[1] / 255.0f), (col[2] / 255.0f),
+              (col[3] / 255.0f));
+  glUniform1f(depthLoc, -1.0f);
+
+  // Setup destination rectangle
+  CRect rect = m_sourceRect;
+  rect.x1 /= renderBuffer->GetWidth();
+  rect.x2 /= renderBuffer->GetWidth();
+  rect.y1 /= renderBuffer->GetHeight();
+  rect.y2 /= renderBuffer->GetHeight();
+
+  PackedVertex vertex[4];
+
+  // Setup vertex position values
+  for (unsigned int i = 0; i < 4; i++)
+  {
+    vertex[i].x = m_rotatedDestCoords[i].x;
+    vertex[i].y = m_rotatedDestCoords[i].y;
+    vertex[i].z = 0.0f;
+  }
+
+  // Setup texture coordinates
+  vertex[0].u1 = vertex[3].u1 = rect.x1;
+  vertex[0].v1 = vertex[1].v1 = rect.y1;
+  vertex[1].u1 = vertex[2].u1 = rect.x2;
+  vertex[2].v1 = vertex[3].v1 = rect.y2;
+
+  glBindVertexArray(m_mainVAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, m_mainVertexVBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(PackedVertex) * 4, &vertex[0], GL_DYNAMIC_DRAW);
+
+  glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_BYTE, nullptr);
+
+  glBindVertexArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  m_context.DisableGUIShader();
 }

eigendude/kodi

xbmc/cores/RetroPlayer/rendering/VideoRenderers/RPRendererDMAOpenGL.cpp
@@ -12,6 +12,7 @@
 #include "cores/RetroPlayer/buffers/RenderBufferPoolDMA.h"
 #include "cores/RetroPlayer/rendering/RenderContext.h"
 #include "cores/RetroPlayer/shaders/gl/ShaderPresetGL.h"
+#include "cores/RetroPlayer/shaders/gl/ShaderTextureGL.h"
 #include "cores/RetroPlayer/shaders/gl/ShaderTextureGLRef.h"
 #include "utils/BufferObjectFactory.h"
 #include "utils/GLUtils.h"
@@ -20,6 +21,8 @@
 
 using namespace KODI;
 using namespace RETRO;
+
+// --- CRendererFactoryDMAOpenGL --------------------------------------------------
 
 std::string CRendererFactoryDMAOpenGL::RenderSystemName() const
 {
@@ -55,37 +58,46 @@ void CRPRendererDMAOpenGL::Render(uint8_t alpha)
   if (renderBuffer == nullptr)
     return;
 
-  RenderBufferTextures* rbTextures;
-  const auto it = m_RBTexturesMap.find(renderBuffer);
-  if (it != m_RBTexturesMap.end())
-  {
-    rbTextures = it->second.get();
-  }
-  else
-  {
-    rbTextures = new RenderBufferTextures{
-        // Source texture
-        std::make_shared<SHADER::CShaderTextureGLRef>(
-            static_cast<unsigned int>(renderBuffer->GetWidth()),
-            static_cast<unsigned int>(renderBuffer->GetHeight()), renderBuffer->TextureID()),
-        // Target texture is empty wrapper used to pass target width and height
-        std::make_shared<SHADER::CShaderTextureGLRef>(
-            static_cast<unsigned int>(m_context.GetScreenWidth()),
-            static_cast<unsigned int>(m_context.GetScreenHeight())),
-    };
-    m_RBTexturesMap.emplace(renderBuffer, rbTextures);
-  }
-
-  std::shared_ptr<SHADER::CShaderTextureGLRef> source = rbTextures->source;
-  std::shared_ptr<SHADER::CShaderTextureGLRef> target = rbTextures->target;
-
   Updateshaders();
 
   // Use video shader preset
   if (m_bUseShaderPreset)
   {
+    RenderBufferTextures* rbTextures;
+
+    // Drop cached textures if target size is changed
+    if (m_fullDestWidth != m_lastTargetWidth || m_fullDestHeight != m_lastTargetHeight)
+    {
+      m_RBTexturesMap.clear();
+      m_lastTargetWidth = m_fullDestWidth;
+      m_lastTargetHeight = m_fullDestHeight;
+    }
+
+    const auto it = m_RBTexturesMap.find(renderBuffer);
+    if (it != m_RBTexturesMap.end())
+    {
+      rbTextures = it->second.get();
+    }
+    else
+    {
+      rbTextures = new RenderBufferTextures{
+          // Source texture
+          std::make_shared<SHADER::CShaderTextureGLRef>(
+              static_cast<unsigned int>(renderBuffer->GetWidth()),
+              static_cast<unsigned int>(renderBuffer->GetHeight()), renderBuffer->TextureID()),
+          // Target texture
+          std::make_shared<SHADER::CShaderTextureGL>(static_cast<unsigned int>(m_fullDestWidth),
+                                                     static_cast<unsigned int>(m_fullDestHeight),
+                                                     GL_UNSIGNED_BYTE, GL_RGBA8, GL_BGRA, false)};
+      rbTextures->target->CreateTexture(); // Create new internal texture
+      m_RBTexturesMap.emplace(renderBuffer, rbTextures);
+    }
+
+    std::shared_ptr<SHADER::CShaderTextureGLRef> source = rbTextures->source;
+    std::shared_ptr<SHADER::CShaderTextureGL> target = rbTextures->target;
+
     GLint filter = GL_NEAREST;
-    if (m_shaderPreset->GetPasses()[0].filterType == SHADER::FilterType::LINEAR)
+    if (m_shaderPreset->GetPasses().front().filterType == SHADER::FilterType::LINEAR)
       filter = GL_LINEAR;
 
     glBindTexture(m_textureTarget, source->GetTextureID());
@@ -94,76 +106,89 @@ void CRPRendererDMAOpenGL::Render(uint8_t alpha)
     glTexParameteri(m_textureTarget, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(m_textureTarget, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-    if (!m_shaderPreset->RenderUpdate(m_rotatedDestCoords, {m_fullDestWidth, m_fullDestHeight},
-                                      *source, *target))
+    if (!m_shaderPreset->RenderUpdate(*source, *target))
     {
       m_bShadersNeedUpdate = false;
       m_bUseShaderPreset = false;
     }
+
+    glActiveTexture(GL_TEXTURE0); // GUI shader samples from texture unit 0
+    glBindTexture(m_textureTarget, target->GetTextureID());
   }
-  // Use GUI shader
   else
   {
     GLint filter = GL_NEAREST;
     if (GetRenderSettings().VideoSettings().GetScalingMethod() == SCALINGMETHOD::LINEAR)
       filter = GL_LINEAR;
 
-    glBindTexture(m_textureTarget, source->GetTextureID());
+    glActiveTexture(GL_TEXTURE0); // GUI shader samples from texture unit 0
+    glBindTexture(m_textureTarget, renderBuffer->TextureID());
     glTexParameteri(m_textureTarget, GL_TEXTURE_MAG_FILTER, filter);
     glTexParameteri(m_textureTarget, GL_TEXTURE_MIN_FILTER, filter);
     glTexParameteri(m_textureTarget, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(m_textureTarget, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-    m_context.EnableGUIShader(GL_SHADER_METHOD::TEXTURE);
-
-    GLint uniColLoc = m_context.GUIShaderGetUniCol();
-    GLint depthLoc = m_context.GUIShaderGetDepth();
-
-    // Setup color values
-    GLubyte col[4];
-    const uint32_t color = (alpha << 24) | 0xFFFFFF;
-    col[0] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::R, color);
-    col[1] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::G, color);
-    col[2] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::B, color);
-    col[3] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::A, color);
-
-    glUniform4f(uniColLoc, (col[0] / 255.0f), (col[1] / 255.0f), (col[2] / 255.0f),
-                (col[3] / 255.0f));
-    glUniform1f(depthLoc, -1.0f);
-
-    // Setup destination rectangle
-    CRect rect = m_sourceRect;
-    rect.x1 /= renderBuffer->GetWidth();
-    rect.x2 /= renderBuffer->GetWidth();
-    rect.y1 /= renderBuffer->GetHeight();
-    rect.y2 /= renderBuffer->GetHeight();
-
-    PackedVertex vertex[4];
-
-    // Setup vertex position values
-    for (unsigned int i = 0; i < 4; i++)
-    {
-      vertex[i].x = m_rotatedDestCoords[i].x;
-      vertex[i].y = m_rotatedDestCoords[i].y;
-      vertex[i].z = 0.0f;
-    }
-
-    // Setup texture coordinates
-    vertex[0].u1 = vertex[3].u1 = rect.x1;
-    vertex[0].v1 = vertex[1].v1 = rect.y1;
-    vertex[1].u1 = vertex[2].u1 = rect.x2;
-    vertex[2].v1 = vertex[3].v1 = rect.y2;
-
-    glBindVertexArray(m_mainVAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, m_mainVertexVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(PackedVertex) * 4, &vertex[0], GL_DYNAMIC_DRAW);
-
-    glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_BYTE, nullptr);
-
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    m_context.DisableGUIShader();
   }
+
+  if (alpha < 255)
+  {
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  }
+  else
+  {
+    glDisable(GL_BLEND);
+  }
+
+  // Use GUI shader
+  m_context.EnableGUIShader(GL_SHADER_METHOD::TEXTURE);
+
+  GLint uniColLoc = m_context.GUIShaderGetUniCol();
+  GLint depthLoc = m_context.GUIShaderGetDepth();
+
+  // Setup color values
+  GLubyte col[4];
+  const uint32_t color = (alpha << 24) | 0xFFFFFF;
+  col[0] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::R, color);
+  col[1] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::G, color);
+  col[2] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::B, color);
+  col[3] = UTILS::GL::GetChannelFromARGB(UTILS::GL::ColorChannel::A, color);
+
+  glUniform4f(uniColLoc, (col[0] / 255.0f), (col[1] / 255.0f), (col[2] / 255.0f),
+              (col[3] / 255.0f));
+  glUniform1f(depthLoc, -1.0f);
+
+  // Setup destination rectangle
+  CRect rect = m_sourceRect;
+  rect.x1 /= renderBuffer->GetWidth();
+  rect.x2 /= renderBuffer->GetWidth();
+  rect.y1 /= renderBuffer->GetHeight();
+  rect.y2 /= renderBuffer->GetHeight();
+
+  PackedVertex vertex[4];
+
+  // Setup vertex position values
+  for (unsigned int i = 0; i < 4; i++)
+  {
+    vertex[i].x = m_rotatedDestCoords[i].x;
+    vertex[i].y = m_rotatedDestCoords[i].y;
+    vertex[i].z = 0.0f;
+  }
+
+  // Setup texture coordinates
+  vertex[0].u1 = vertex[3].u1 = rect.x1;
+  vertex[0].v1 = vertex[1].v1 = rect.y1;
+  vertex[1].u1 = vertex[2].u1 = rect.x2;
+  vertex[2].v1 = vertex[3].v1 = rect.y2;
+
+  glBindVertexArray(m_mainVAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, m_mainVertexVBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(PackedVertex) * 4, &vertex[0], GL_DYNAMIC_DRAW);
+
+  glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_BYTE, nullptr);
+
+  glBindVertexArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  m_context.DisableGUIShader();
 }